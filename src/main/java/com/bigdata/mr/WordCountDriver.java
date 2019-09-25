44a567c98845ed92258858afc054af69a4bffffd
2015015279 <qaz852701882@foxmail.com>
2019-09-26 00:06:44 +0800

    MR学习

--- src/main/java/com/bigdata/mr/WordCountDriver.java
+++ src/main/java/com/bigdata/mr/WordCountDriver.java
@@ -8,7 +8,6 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-import java.io.IOException;
 
 /**
  * @author : wangshengyu
