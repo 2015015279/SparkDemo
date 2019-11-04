fffa21a8d99e7c47c8641445c1b7a026932b4e6e
wangshengyu4 <wangshengyu1@jd.com>
2019-11-04 17:48:13 +0800

    redis 学习

--- src/main/java/com/bigdata/redis/list/ListMain.java
+++ src/main/java/com/bigdata/redis/list/ListMain.java
@@ -46,51 +46,51 @@ public class ListMain {
             System.out.print(name + "  ");
         }
         System.out.println();
-//
-//        //剧情：插队不文明，为了遏制这种不文明的现象，大决决定打一架。  鸠摩智被打跑了。
-//        jedis.lpop("柜台1");
-//        for (String name : jedis.lrange("柜台1", 0, -1)) {
-//            System.out.print(name + "  ");
-//        }
-//        System.out.println();
-//
-//        //剧情：慕容复一看情况不好，以表哥的身份忽悠王语嫣，把王语嫣打伤。
-//        jedis.rpop("柜台1");
-//        for (String name : jedis.lrange("柜台1", 0, -1)) {
-//            System.out.print(name + "  ");
-//        }
-//        System.out.println();
-//
-//        //剧情：在大家打架的时候，我偷偷插队，买了肯德基。
-//        jedis.rpush("柜台1", "井中月");
-//        for (String name : jedis.lrange("柜台1", 0, -1)) {
-//            System.out.print(name + "  ");
-//        }
-//        System.out.println();
-//
-//        //剧情；等我买了肯德基，慕容复被打跑了
-//        jedis.lpop("柜台1");
-//        for (String name : jedis.lrange("柜台1", 0, -1)) {
-//            System.out.print(name + "  ");
-//        }
-//        System.out.println();
-//
-//        //剧情：星宿老怪 突然来了，把 阿紫和游坦之同时弄走了。
-//        String result = jedis.ltrim("柜台1", 2, 5);
-//        if ("OK".equals(result)) {
-//            for (String name : jedis.lrange("柜台1", 0, -1)) {
-//                System.out.print(name + "  ");
-//            }
-//        }
-//        System.out.println("");
-//
-//        //剧情：这时候，乔峰三人发现了我，与我大战三百回合，我全身而退
-//        String res = jedis.ltrim("柜台1", 0, 2);
-//        if ("OK".equals(res)) {
-//            for (String name : jedis.lrange("柜台1", 0, -1)) {
-//                System.out.print(name + "  ");
-//            }
-//        }
+
+        //剧情：插队不文明，为了遏制这种不文明的现象，大决决定打一架。  鸠摩智被打跑了。
+        jedis.lpop("柜台1");
+        for (String name : jedis.lrange("柜台1", 0, -1)) {
+            System.out.print(name + "  ");
+        }
+        System.out.println();
+
+        //剧情：慕容复一看情况不好，以表哥的身份忽悠王语嫣，把王语嫣打伤。
+        jedis.rpop("柜台1");
+        for (String name : jedis.lrange("柜台1", 0, -1)) {
+            System.out.print(name + "  ");
+        }
+        System.out.println();
+
+        //剧情：在大家打架的时候，我偷偷插队，买了肯德基。
+        jedis.rpush("柜台1", "井中月");
+        for (String name : jedis.lrange("柜台1", 0, -1)) {
+            System.out.print(name + "  ");
+        }
+        System.out.println();
+
+        //剧情；等我买了肯德基，慕容复被打跑了
+        jedis.lpop("柜台1");
+        for (String name : jedis.lrange("柜台1", 0, -1)) {
+            System.out.print(name + "  ");
+        }
+        System.out.println();
+
+        //剧情：星宿老怪 突然来了，把 阿紫和游坦之同时弄走了。
+        String result = jedis.ltrim("柜台1", 2, 5);
+        if ("OK".equals(result)) {
+            for (String name : jedis.lrange("柜台1", 0, -1)) {
+                System.out.print(name + "  ");
+            }
+        }
+        System.out.println("");
+
+        //剧情：这时候，乔峰三人发现了我，与我大战三百回合，我全身而退
+        String res = jedis.ltrim("柜台1", 0, 2);
+        if ("OK".equals(res)) {
+            for (String name : jedis.lrange("柜台1", 0, -1)) {
+                System.out.print(name + "  ");
+            }
+        }
 
     }
 }
